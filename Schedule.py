7ce36260db301538091323bb0d8ff3260f13f91a
xlWang <1448039778@qq.com>
2024-01-05 17:48:30 +0800

    完成

--- Schedule.py
+++ Schedule.py
@@ -2,7 +2,9 @@ import datetime
 import sxtwl
 import smtplib
 import email,ssl
-from email.message import EmailMessage
+from email.header import Header
+from email.mime.text import MIMEText
+import logging
 def getlunar_day():
     tomorrow = str(datetime.date.today() + datetime.timedelta(days=1))
     solar_day = tomorrow
@@ -13,33 +15,46 @@ def getlunar_day():
     lunar_date_tomorrow = "%d-%s%d-%d" % (solar_day.getLunarYear(),
                         '闰' if solar_day.isLunarLeap() else '', solar_day.getLunarMonth(), solar_day.getLunarDay())
     return lunar_date_tomorrow
-def gettoday():
+def Comparison():
     tomorrow = getlunar_day()
-    person = []
     with open('./info.txt',encoding='utf-8') as f:
         for info in f:
             info = info.strip().split(',')
             name = info[0]
             month_and_day = info[1]
-            if info[1] == tomorrow[5:]:
-                person.append([name,str(datetime.date.today() + datetime.timedelta(days=1))])
-        print(person)
-        return person
 
-def sendmessage():
+            if info[1] == tomorrow[5:]:
+                date = str(datetime.date.today() + datetime.timedelta(days=1))
+                sendmessage(name,month_and_day,date)
+            else:
+                print("nothing")
+
+def sendmessage(name,month_and_day,date):
     with open('./授权码.txt', encoding='utf-8') as i:
         info = i.read()
         info = info.strip().split(',')
-        EMAIL_ADDRESS = info[0]
-        EMAIL_PASSWORD = info[1]
-        receive = info[2]
-    context = ssl.create_default_context()
-    smtp = smtplib.SMTP_SSL("smtp.163.com", 465, context=context)
+        EMAIL_ADDRESS = info[0] #发送人
+        EMAIL_PASSWORD = info[1] #密码
+        receive = info[2] #接受人
+    mail_content = f"助手提醒：明天是{date},农历{month_and_day},{name}的生日~~"
+    smtp_server = 'smtp.163.com'
+    subject = "生日提醒"
+    msg = MIMEText(mail_content, 'html', 'utf-8')
+    msg['subject'] = Header(subject, 'utf-8')
+    msg['From'] = EMAIL_ADDRESS
+    msg['To'] = receive
+    smtp = smtplib.SMTP_SSL(smtp_server, 465)
+    smtp.helo(smtp_server)
+    smtp.ehlo(smtp_server)
     smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
-    sender = EMAIL_ADDRESS
+    logging.info("Start send Email....")
+    smtp.sendmail(EMAIL_ADDRESS, receive, msg.as_string())
+    smtp.quit()
+    logging.info("Send End!")
 
+    
 
 if __name__ == '__main__':
-    # gettoday()
-    sendmessage()
+   Comparison()
+
 
