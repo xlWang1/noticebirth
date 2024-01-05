a04c49b2fea8add62304982d05e603bf16da275d
xlWang <1448039778@qq.com>
2024-01-05 17:31:39 +0800

    日常更新

--- Schedule.py
+++ Schedule.py
@@ -1,34 +1,45 @@
 import datetime
 import sxtwl
+import smtplib
+import email,ssl
+from email.message import EmailMessage
+def getlunar_day():
+    tomorrow = str(datetime.date.today() + datetime.timedelta(days=1))
+    solar_day = tomorrow
+    year = int(solar_day[:4])
+    month = int(solar_day[5:7])
+    day = int(solar_day[8:])
+    solar_day = sxtwl.fromSolar(year,month,day)
+    lunar_date_tomorrow = "%d-%s%d-%d" % (solar_day.getLunarYear(),
+                        '闰' if solar_day.isLunarLeap() else '', solar_day.getLunarMonth(), solar_day.getLunarDay())
+    return lunar_date_tomorrow
 def gettoday():
-    today = str(datetime.date.today())
-    today_year = int(today[:4])
-    today_month = int(today[5:7])
-    today_day = int(today[8:])
-    today = str(today_year)+'-'+str(today_month)+'-'+str(today_day)
-    print("今天是" + today)
+    tomorrow = getlunar_day()
     person = []
     with open('./info.txt',encoding='utf-8') as f:
         for info in f:
             info = info.strip().split(',')
             name = info[0]
             month_and_day = info[1]
-            this_birthday = str(today_year-1)+month_and_day
-            this_birthday = getsolar(this_birthday)
-            print(this_birthday)
-            if today == this_birthday:
-                person.append([name,today_day])
-                print(person)
-                return person
-def getsolar(lunar_day):
-    year = int(lunar_day[:4])
-    month = int(lunar_day[4:6])
-    day = int(lunar_day[6:])
-    lunar_day = sxtwl.fromLunar(year,month,day)
-    solar_date = "%d-%d-%d" % (lunar_day.getSolarYear(), lunar_day.getSolarMonth(), lunar_day.getSolarDay())
-    return solar_date
+            if info[1] == tomorrow[5:]:
+                person.append([name,str(datetime.date.today() + datetime.timedelta(days=1))])
+        print(person)
+        return person
+
+def sendmessage():
+    with open('./授权码.txt', encoding='utf-8') as i:
+        info = i.read()
+        info = info.strip().split(',')
+        EMAIL_ADDRESS = info[0]
+        EMAIL_PASSWORD = info[1]
+        receive = info[2]
+    context = ssl.create_default_context()
+    smtp = smtplib.SMTP_SSL("smtp.163.com", 465, context=context)
+    smtp.login(EMAIL_ADDRESS, EMAIL_PASSWORD)
+    sender = EMAIL_ADDRESS
+
 
 if __name__ == '__main__':
-    # print(getsolar('20231123'))
-    gettoday()
+    # gettoday()
+    sendmessage()
 
