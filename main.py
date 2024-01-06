eb0f2803b3d2c7fb09d943b7caeade659a236a8e
xlWang <1448039778@qq.com>
2024-01-06 13:05:51 +0800

    参数化地址

--- main.py
+++ main.py
@@ -5,19 +5,22 @@
 import pushweather
 import Schedule
 import time
-def start_run(city):
+def start_run(city,info_add,author_add):
     try:
         pushweather.GetWxToken()
         pushweather.GetWheatherInfo(city)
     except Exception as err1:
         print(err1)
     try:
-        Schedule.Comparison()
+        Schedule.Comparison(info_add,author_add)
     except Exception as err2:
         print(err2)
 
 if __name__ == '__main__':
-    start_run(['嘉兴市南湖区', '成都市武侯区'])
+    info_add = './info.txt'
+    author_add = './authorization.txt'
+    city = ['嘉兴市南湖区', '成都市武侯区']
+    start_run(city,info_add,author_add)
 
 
 
